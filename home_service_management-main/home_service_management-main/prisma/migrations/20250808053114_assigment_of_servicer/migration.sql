6bbaf9175001ffffe6f910459efc933f7c682937
mohit-enacton <mmsharma@enacton.email>
2025-08-26 17:28:08 +0530

    Delete home_service_management-main/home_service_management-main directory

--- home_service_management-main/home_service_management-main/prisma/migrations/20250808053114_assigment_of_servicer/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `servicerequest` MODIFY `scheduledAt` DATETIME(3) NULL;
--- home_service_management-main/home_service_management-main/src/modules/requests/dto/assign-provider.dto.ts
+++
@@ -1,4 +0,0 @@
-export interface AssignProviderDto {
-  requestId: number;
-  providerId: number;
-}
